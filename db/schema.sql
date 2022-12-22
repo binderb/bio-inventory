d54933f3f66a04bfac767a27e4ef2daf78ac34d8
Ben Binder <ben.binder11@gmail.com>
2022-12-22 14:36:33 -0600

    Add basic server architecture.

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS bioinventory_db;
+CREATE DATABASE bioinventory_db;
---
+++ server.js
@@ -0,0 +1,31 @@
+// Dependencies
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const sequelize = require('./config/connection');
+
+// Define server and port
+const PORT = 3001;
+const app = express();
+
+// Enable base middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Enable sessions
+
+// Enable template engine
+
+// Enable modular routing
+
+// Provide catch-all routes
+
+// Require sequelize model sync before listening
+async function init () {
+  await sequelize.sync({force: false});
+  app.listen(PORT, () => console.log(`BioInventory now listening on port ${PORT}. ✨🧪✨`));
+}
+
+// Initialize
+init();
