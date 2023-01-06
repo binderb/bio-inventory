143a9bb4c83d79efbbf2b31f830e4545ff7f8abc
Ben Binder <ben.binder@switchbackmedical.com>
2023-01-06 15:10:16 -0600

    Add new inventory models to index file.

--- models/index.js
+++ models/index.js
@@ -1,7 +1,16 @@
 const User = require('./User');
-
+const Spec = require('./Spec');
+const Item = require('./Item');
+const Category = require('./Category');
+const Location = require('./Location');
+const Vendor = require('./Vendor');
 
 
 module.exports = {
-  User
+  User,
+  Spec,
+  Item,
+  Category,
+  Location,
+  Vendor
 }
