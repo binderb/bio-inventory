58cf3f16238de2d12a5831483c114d3202d4732c
Ben Binder <ben.binder@switchbackmedical.com>
2023-01-26 16:21:49 -0600

    Add QR scan support.

--- public/js/manageDashboardMenu.js
+++ public/js/manageDashboardMenu.js
@@ -1,7 +1,13 @@
 const nav = document.querySelector('.inventory-subnav-group')
 
 function hideAllPanels() {
+  try {
+    window.html5QrCode.stop();
+  } catch (err) {
+    // QR scanner is not running, no need to stop it.
+  }
   document.querySelector('#quick-search-panel').setAttribute('style','display:none;');
+  document.querySelector('#scan-panel').setAttribute('style','display:none;');
   document.querySelector('#create-spec-panel').setAttribute('style','display:none;');
   document.querySelector('#create-item-panel').setAttribute('style','display:none;');
   document.querySelector('#create-location-panel').setAttribute('style','display:none;');
@@ -19,6 +25,10 @@ function switchToPanel (id) {
     switch (id) {
       case 'search':
         document.querySelector('#quick-search-panel').setAttribute('style','');
+        break;
+      case 'scan':
+        document.querySelector('#scan-panel').setAttribute('style','');
+        window.html5QrCode.start({facingMode: 'environment'}, config, window.qrCodeSuccessCallback);
         break;
       case 'add-spec':
         document.querySelector('#create-spec-panel').setAttribute('style','');
---
+++ public/js/manageQRScan.js
@@ -0,0 +1,42 @@
+let video_constraints = {
+  audio: false,
+  video: {
+    width: {
+      ideal: 250
+    },
+    height: {
+      ideal: 250
+    },
+    facingMode: 'environment',
+    focusMode: 'continuous',
+    aspectRatio: 1
+  }
+}
+var config = {
+  fps: 10,
+  qrbox: {width: 200, height: 200},
+  rememberLastUsedCamera: true,
+  supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA],
+  aspectRatio: 1
+
+}
+
+window.html5QrCode = new Html5Qrcode(
+  "reader", { formatsToSupport: [ Html5QrcodeSupportedFormats.QR_CODE ] });
+window.qrCodeSuccessCallback = async (decodedText, decodedResult) => {
+  const decodedObject = JSON.parse(decodedText);
+  if (Object.keys(decodedObject).length == 2) {
+    window.html5QrCode.stop();
+    if (Object.keys(decodedObject)[1] == 'spec_id') {
+      window.location.replace(`/specs/${decodedObject.spec_id}/items/${decodedObject.id}`);
+    } else {
+      // Alternate solution, to support legacy implementations of the QR code payload format.
+      const pn = decodedObject[Object.keys(decodedObject)[1]];
+      const specResponse = await fetch(`/api/specs/pn/${pn}`);
+      if (specResponse.ok) {
+        const specData = await specResponse.json();
+        window.location.replace(`/specs/${specData.id}/items/${decodedObject.id}`);
+      }
+    }
+  }
+};
